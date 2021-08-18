4e1bc94930f5ae8d7f2ce280179200de3e0063e9
Dai Kindaichi <kindai.0911@gmail.com>
2021-08-18 20:58:02 +0900

    go get -u github.com/gin-gonic/gin and add root path and tamplate

--- server/main.go
+++ server/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello world!")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{
+			"title": "twitter-clone-go",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
